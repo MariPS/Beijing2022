f82a3eb1e0638e88f9918d974ced2555fa86a8ed
MariPS <salvatomarikapia@gmail.com>
2022-02-15 16:12:05 +0100

    backennd: aggiornamento query di accesso al db (modifica degli endpoints e aggiunta query per il prelevamento dei nomi delle discipline)

--- webapp/backend/server/database.py
+++ webapp/backend/server/database.py
@@ -9,6 +9,8 @@ database = client.Beijing2022
 disciplines_collection = database.get_collection("disciplines")
 medals_collection = database.get_collection("medals")
 results_collection = database.get_collection("results")
+events_collection = database.get_collection("events")
+
 
 # Retrieve all disciplines documents
 async def retrieve_all_disciplines():
@@ -18,9 +20,18 @@ async def retrieve_all_disciplines():
     return documents
 
 # Retrieve discipline by name
-async def retrieve_discipline_by_name(discipline):
+async def retrieve_discipline_by_name(discipline: str):
     document = await disciplines_collection.find_one({"Discipline": discipline},{"_id":0})
     return document
+
+# Retrieve disciplines' name list
+async def retrieve_disciplines_names():
+    disciplines_names = []
+    async for document in disciplines_collection.find({},{"_id":0, "M":0, "F":0, "Total":0, "athletes":0}):
+        disciplines_names.append(document["Discipline"])
+    return disciplines_names
+
+
 
 # Retrieve all medals documents
 async def retrieve_all_medals():
@@ -28,3 +39,17 @@ async def retrieve_all_medals():
     async for medal in medals_collection.find({},{"_id":0}):
         medals.append(medal)
     return medals
+
+# Retrieve all results
+async def retrieve_all_results():
+    results = []
+    async for result in results_collection.find({},{"_id":0}):
+        results.append(result)
+    return results
+
+# Retrieve all events
+async def retrieve_all_events():
+    events = []
+    async for event in events_collection.find({},{"_id":0}):
+        events.append(event)
+    return events
--- webapp/backend/server/routes.py
+++ webapp/backend/server/routes.py
@@ -1,29 +1,42 @@
+from unittest import result
 from fastapi import APIRouter
 import server.database as db
 
 router = APIRouter()
 
 
-@router.get("/", tags=["Root"])
-async def read_root():
-    disciplines = await db.retrieve_all_disciplines()
-    return disciplines
+# @router.get("/", tags=["Root"])
 
 
-@router.get("/disciplines")
+
+@router.get("/disciplines", tags=["Disciplines"])
 async def get_all_disciplines():
     disciplines = await db.retrieve_all_disciplines()
     if disciplines:
         return disciplines
 
-@router.get("/medals")
-async def get_all_medals():
-    medals = await db.retrieve_all_medals()
-    if medals:
-        return medals
+@router.get("/disciplines/names", tags=["Disciplines"])
+async def get_disciplines_names():
+    disciplines = await db.retrieve_disciplines_names()
+    if disciplines:
+        return disciplines
 
 @router.get("/disciplines/{disciplineName}", tags=["Disciplines"])
 async def get_discipline_by_name(disciplineName : str):
     discipline = await db.retrieve_discipline_by_name(disciplineName)
     if discipline:
         return discipline
+
+
+@router.get("/medals", tags=["Medals"])
+async def get_all_medals():
+    medals = await db.retrieve_all_medals()
+    if medals:
+        return medals
+
+
+@router.get("/events", tags=["Events"])
+async def get_all_events():
+    events = await db.retrieve_all_events()
+    if events:
+        return events
