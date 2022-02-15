a2dbd6dc543c3e730b33f65163664ba45df4aee4
MariPS <salvatomarikapia@gmail.com>
2022-02-15 16:10:33 +0100

    database: aggiornamento dello script di popolamento per l'aggiunta del dataset events.json

--- file_popolamento_db/script_popolamento.sh
+++ file_popolamento_db/script_popolamento.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mongoimport --db=Beijing2022 --collection=disciplines --file=disciplines.json
 mongoimport --db=Beijing2022 --collection=medals --file=medals.json
-mongoimport --db=Beijing2022 --collection=results --file=results.json
+mongoimport --db=Beijing2022 --collection=events --file=events.json
